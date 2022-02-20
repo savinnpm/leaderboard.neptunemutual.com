c1e885f7771474ebb4ae651fd205ad4e6a51b2c4
flashburst <82953782+flashburst@users.noreply.github.com>
2022-02-20 20:56:40 +0530

    Install Tailwind CSS

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <div className="   p-0   sm:p-0 "></div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
