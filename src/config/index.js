317341e403ec3bddc949c88124b98d59cfbc8b9e
tokyo <tokyo@neptunemutual.com>
2022-03-03 23:55:23 +0530

    Updated API Server

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const PAGE_TITLE = "Leaderboard - Neptune Mutual";
 export const PAGE_DESCRIPTION = "Reduce Your Exposure to Crypto Market Risks";
 
-export const BASE_URL = "https://api.leaderboard.neptunemutual.com";
+export const BASE_URL = "https://api.neptunemutual.com";
 export const BUG_REPORT_URL =
   "https://forms.clickup.com/f/6vvfh-5464/CEMFTSGE5Y715W7RRG";
 
