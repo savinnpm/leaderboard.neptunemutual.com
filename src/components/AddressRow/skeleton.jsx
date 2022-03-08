bc85b767b91c4b78d5de55c17edb102bd09d233d
flashburst <82953782+flashburst@users.noreply.github.com>
2022-03-08 17:21:00 +0530

    Fix column widths

--- src/components/AddressRow/skeleton.jsx
+++ src/components/AddressRow/skeleton.jsx
@@ -6,35 +6,37 @@ export function AddressRowSkeleton() {
   return (
     <tr className={styles.row}>
       <td className={classNames(styles.rank_cell)}>
-        <span
-          className={classNames(styles.rank_content_skel, styles.skeleton)}
-        ></span>
+        <div className={classNames(styles.rank_cell_skel, styles.skeleton)}>
+          &nbsp;
+        </div>
       </td>
 
       <td className={styles.image_cell}>
-        <div
-          className={classNames(styles.image_cell_content, styles.skeleton)}
-        ></div>
+        <div className={styles.image_wrapper}>
+          <div className={classNames(styles.image_cell_skel, styles.skeleton)}>
+            &nbsp;
+          </div>
+        </div>
       </td>
 
       <td className={classNames(styles.name_cell, styles.name_cell_skel)}>
-        <h3 className={classNames(styles.skeleton)}>.</h3>
+        <h3 className={classNames(styles.skeleton)}>&nbsp;</h3>
       </td>
 
       <td className={styles.address_cell}>
-        <span className={classNames(styles.address_cell_skel, styles.skeleton)}>
-          0xbfc6129b6191f757fe334e1fd07d56b3cb57062e
-        </span>
+        <div className={classNames(styles.address_cell_skel, styles.skeleton)}>
+          &nbsp;
+        </div>
       </td>
 
       <td className={styles.points_cell}>
-        <span className={classNames(styles.points_cell_skel, styles.skeleton)}>
-          180
-        </span>
+        <div className={classNames(styles.points_cell_skel, styles.skeleton)}>
+          &nbsp;
+        </div>
       </td>
 
-      <td className={styles.arrow_skel}>
-        <ArrowRightIcon height={18} />
+      <td className={styles.action_cell}>
+        <ArrowRightIcon height={18} fill="transparent" />
       </td>
     </tr>
   );
---
+++ src/utils/address.js
@@ -0,0 +1,3 @@
+export const isZeroAddress = (address) => {
+  return address === "0x0000000000000000000000000000000000000000";
+};
