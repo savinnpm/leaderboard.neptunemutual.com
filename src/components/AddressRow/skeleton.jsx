17f0bc59c73a91c08ad5a7a2b2cb30391a3c9ba2
rupak-nm <rupak@neptunemutual.com>
2022-03-08 15:43:28 +0545

    ui changes

--- src/components/AddressRow/skeleton.jsx
+++ src/components/AddressRow/skeleton.jsx
@@ -17,21 +17,19 @@ export function AddressRowSkeleton() {
         ></div>
       </td>
 
-      <td className={classNames(styles.name_cell)}>
-        <h3 className={classNames(styles.name_cell_skel, styles.skeleton)}>
-          .
-        </h3>
+      <td className={classNames(styles.name_cell, styles.name_cell_skel)}>
+        <h3 className={classNames(styles.skeleton)}>.</h3>
       </td>
 
       <td className={styles.address_cell}>
         <span className={classNames(styles.address_cell_skel, styles.skeleton)}>
-          0x0000000000000000000000000000000000000000
+          0xbfc6129b6191f757fe334e1fd07d56b3cb57062e
         </span>
       </td>
 
       <td className={styles.points_cell}>
         <span className={classNames(styles.points_cell_skel, styles.skeleton)}>
-          .
+          180
         </span>
       </td>
 
